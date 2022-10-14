eedce827529c160444aeaa5fe0f6b55d7a997e2d
estuardo8u14 <estuardo8u14@gmail.com>
2022-10-13 21:23:51 -0600

    pruebaapi

--- jscript/predict.js
+++ jscript/predict.js
@@ -83,10 +83,12 @@ $("#image-selector").change(function () {
 // == Loads the model and makes a prediction on the default image. == //
 let model;
 (async function () {
-  model = await tf.loadModel("http://brain.test.woza.work/model_3/model.json");
+  model = await tf.loadModel(
+    "https://ahcstroke.herokuapp.com/model_3/model.json"
+  );
   $("#selected-image").attr(
     "src",
-    "http://brain.test.woza.work/assets/ich.jpg"
+    "https://ahcstroke.herokuapp.com/assets/ich.jpg"
   );
 
   // Hide the model loading spinner
