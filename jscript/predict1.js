4b40a1a6d12938432254f44089de3d570e5e58ee
estuardo8u14 <estuardo8u14@gmail.com>
2022-10-14 16:27:10 -0600

    fin

--- jscript/predict1.js
+++ jscript/predict1.js
@@ -5,6 +5,14 @@ function simulateClick(tabID) {
 function predictOnLoad() {
   setTimeout(simulateClick.bind(null, "predict-button"), 500);
 }
+var timeleft = 15;
+var downloadTimer = setInterval(function () {
+  if (timeleft <= 0) {
+    clearInterval(downloadTimer);
+  }
+  document.getElementById("progressBar").value = 15 - timeleft;
+  timeleft -= 1;
+}, 1000);
 
 $(".spinner").hide();
 
